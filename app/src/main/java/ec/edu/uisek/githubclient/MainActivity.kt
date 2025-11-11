6927d445b97c221005a8fe459a851fa9b5c8da22
Usuario invitado <Guest@iMacdePCEA10804.PUCE.EDU.EC>
2025-11-11 14:51:10 -0500

    muestra de info en la app

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -8,10 +8,22 @@ import androidx.core.view.WindowInsetsCompat
 import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    //comienza la magia
+    private lateinit var reposAdapter: ReposAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         //lateinit se inicializa despues propio de kotlin
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecylerView()
+    }
+
+    private fun setupRecylerView(){
+        reposAdapter = ReposAdapter()
+        binding.repoRecyclerView.adapter = reposAdapter
     }
 }
+
